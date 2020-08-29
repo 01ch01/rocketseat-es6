476d097eaefb61bcda6679995f9ea39ad8f4f2f2
01ch01 <00claudio.henrique@gmail.com>
2020-08-29 18:56:02 -0300

    structuring html

--- main.js
+++
@@ -1,6 +0,0 @@
-alert('msg');
-
-class Person {
-  method() {}
-  other() {}
-}
---
+++ src/main.js
@@ -0,0 +1,57 @@
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: 'unform',
+      description: 'Easy peasy highly scalable ReactJS & React Native forms!',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
+      html_url: 'https://github.com/Rocketseat/unform',
+    });
+
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repos.forEach((repo) => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Access'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
+  }
+}
+
+new App();
